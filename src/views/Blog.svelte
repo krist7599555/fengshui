8381b9321607778dec044ee74f55806a3dd2b66d
krist7599555 <krist7599555@gmail.com>
2019-08-24 17:52:49 +0700

    first commit

---
+++ src/views/Blog.svelte
@@ -0,0 +1,11 @@
+<section class="section">
+  <div class="container">
+    <h1>Blog</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorem
+      laborum beatae ab illo corrupti omnis, veritatis quam pariatur nulla quas
+      hic aliquam! Officiis obcaecati, rem necessitatibus voluptatum similique
+      atque.
+    </p>
+  </div>
+</section>
